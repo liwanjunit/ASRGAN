27d81e550cabc0f49d4253aeb9c9de411c13c5ea
liwanjunit <liwanjunit@163.com>
2022-04-04 01:16:38 +0800

    4.4

--- loss.py
+++ loss.py
@@ -7,8 +7,8 @@ class GeneratorLoss(nn.Module):
     def __init__(self):
         super(GeneratorLoss, self).__init__()
         vgg = vgg16(pretrained=True)
-        # loss_network = nn.Sequential(*list(vgg.features)[:31]).eval()
-        loss_network = nn.Sequential(*list(vgg.features)[:35]).eval()
+        loss_network = nn.Sequential(*list(vgg.features)[:31]).eval()
+        # loss_network = nn.Sequential(*list(vgg.features)[:35]).eval()
         for param in loss_network.parameters():
             param.requires_grad = False
         self.loss_network = loss_network
@@ -20,11 +20,11 @@ class GeneratorLoss(nn.Module):
         # Adversarial Loss
         adversarial_loss = torch.mean(1 - out_labels)
         # Perception Loss
-        # perception_loss = self.mse_loss(self.loss_network(out_images), self.loss_network(target_images))
-        perception_loss = self.charbonnier_loss(self.loss_network(out_images), self.loss_network(target_images))
+        perception_loss = self.mse_loss(self.loss_network(out_images), self.loss_network(target_images))
+        # perception_loss = self.charbonnier_loss(self.loss_network(out_images), self.loss_network(target_images))
         # Image Loss
-        # image_loss = self.mse_loss(out_images, target_images)
-        image_loss = self.charbonnier_loss(out_images, target_images)
+        image_loss = self.mse_loss(out_images, target_images)
+        # image_loss = self.charbonnier_loss(out_images, target_images)
         # TV Loss
         tv_loss = self.tv_loss(out_images)
         return image_loss + 0.001 * adversarial_loss + 0.006 * perception_loss + 2e-8 * tv_loss
