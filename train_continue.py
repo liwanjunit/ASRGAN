bb3a57089261bc11e1ff56c3eedfa023150b5f84
liwanjunit <liwanjunit@163.com>
2022-04-04 11:18:32 +0800

    4.4

--- train_continue.py
+++ train_continue.py
@@ -23,15 +23,15 @@ if __name__ == '__main__':
 
     CROP_SIZE = 128
     UPSCALE_FACTOR = 4
-    NUM_EPOCHS = 30
-    EPOCH_SUM = 70
+    NUM_EPOCHS = 25
+    EPOCH_SUM = 100
 
     D_INIT_LR = 0.0001
     G_INIT_LR = 0.0001
     BATCH_SIZE = 2
 
-    MODEL_NAME_G = 'srgan_netG_epoch_4_70.pth'
-    MODEL_NAME_D = 'srgan_netD_epoch_4_70.pth'
+    MODEL_NAME_G = 'srgan_netG_epoch_4_100.pth'
+    MODEL_NAME_D = 'srgan_netD_epoch_4_100.pth'
 
     print(f'epoch_sum:{EPOCH_SUM}')
     print(f'batch_size:{BATCH_SIZE}')
