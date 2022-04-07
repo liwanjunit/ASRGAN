d550596e33e2e898ec793ff62febb8ae8d0f3c9a
liwanjunit <liwanjunit@163.com>
2022-04-07 18:37:26 +0800

    4.7

--- test/test_psnr_ssim.py
+++ test/test_psnr_ssim.py
@@ -34,14 +34,14 @@ if __name__ == '__main__':
     if not os.path.exists(out_path):
         os.makedirs(out_path)
 
-    for i in range(10):
+    for i in range(40):
 
         index = 1
         psnr_sum = 0
         ssim_sum = 0
 
         # MODEL_NAME = f'C:/code/train_results/model/t_x4/G/tsrgan_netG_epoch_4_{i+30}.pth'
-        MODEL_NAME = f'C:/code/train_results/new_model/tsrgan_x4/G/tsrgan_netG_epoch_4_{i+1}.pth'
+        MODEL_NAME = f'C:/code/train_results/new_model/tsrgan_x4/G/tsrgan_netG_epoch_4_{i+10}.pth'
         # MODEL_NAME = f'C:/code/SRCNN_Pytorch_1.0-master/SRCNN_Pytorch_1.0-master/outputs/x4/epoch_{i+1}.pth'
 
         model = Generator_TSRGAN(UPSCALE_FACTOR).eval()
@@ -93,7 +93,7 @@ if __name__ == '__main__':
     data_frame = pd.DataFrame(
         data={'PSNR': psnr_set, 'SSIM': ssim_set},
         index=range(1, epoch_sum))
-    data_frame.to_csv(out_path + 'tsrgan_val_' + str(UPSCALE_FACTOR) + '.csv', index_label='Epoch')
+    data_frame.to_csv(out_path + 'tsrgan_test_' + str(UPSCALE_FACTOR) + '.csv', index_label='Epoch')
 
     x = range(1, epoch_sum)
 
