5f41868cddaf2e97aae10c80b274cf2789e7d4e8
liwanjunit <liwanjunit@163.com>
2022-04-16 00:19:04 +0800

    4.16

--- train_tsrgan.py
+++ train_tsrgan.py
@@ -18,16 +18,16 @@ from model.model_tsrgan import Generator_TSRGAN, Discriminator_TSRGAN
 if __name__ == '__main__':
 
     CROP_SIZE = 128
-    UPSCALE_FACTOR = 4
-    NUM_EPOCHS = 15
-    EPOCH_SUM = 185
+    UPSCALE_FACTOR = 2
+    NUM_EPOCHS = 30
+    EPOCH_SUM = 0
     BATCH_SIZE = 2
 
     D_INIT_LR = 0.0001
     G_INIT_LR = 0.0001
 
-    MODEL_NAME_G = 'tsrgan_netG_epoch_4_185.pth'
-    MODEL_NAME_D = 'tsrgan_netD_epoch_4_185.pth'
+    # MODEL_NAME_G = 'tsrgan_netG_epoch_4_185.pth'
+    # MODEL_NAME_D = 'tsrgan_netD_epoch_4_185.pth'
 
     print(f'epoch_sum:{EPOCH_SUM}')
     print(f'batch_size:{BATCH_SIZE}')
@@ -56,11 +56,11 @@ if __name__ == '__main__':
         netG.cuda()
         netD.cuda()
         generator_criterion.cuda()
-        netG.load_state_dict(torch.load('epochs/' + MODEL_NAME_G), False)
-        netD.load_state_dict(torch.load('epochs/' + MODEL_NAME_D), False)
-    else:
-        netG.load_state_dict(torch.load('epochs/' + MODEL_NAME_G, map_location=lambda storage, loc: storage))
-        netD.load_state_dict(torch.load('epochs/' + MODEL_NAME_D, map_location=lambda storage, loc: storage))
+    #     netG.load_state_dict(torch.load('epochs/' + MODEL_NAME_G), False)
+    #     netD.load_state_dict(torch.load('epochs/' + MODEL_NAME_D), False)
+    # else:
+    #     netG.load_state_dict(torch.load('epochs/' + MODEL_NAME_G, map_location=lambda storage, loc: storage))
+    #     netD.load_state_dict(torch.load('epochs/' + MODEL_NAME_D, map_location=lambda storage, loc: storage))
 
     optimizerG = optim.Adam(netG.parameters(), lr=G_INIT_LR)
     optimizerD = optim.Adam(netD.parameters(), lr=D_INIT_LR)
