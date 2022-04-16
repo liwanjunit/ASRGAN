27e569e2a8df82deca92fa6a5085d9f0a92b7cbf
liwanjunit <liwanjunit@163.com>
2022-04-17 00:21:03 +0800

    4.17

--- data_utils.py
+++ data_utils.py
@@ -59,7 +59,7 @@ class TrainDatasetFromFolder(Dataset):
         hr_image = self.hr_transform(Image.open(self.image_filenames[index]))
         lr_image = self.lr_transform(hr_image)
         bicubic_image = self.bicubic_transform(lr_image)
-        return bicubic_image, hr_image
+        # return bicubic_image, hr_image
         return lr_image, bicubic_image, hr_image
 
     def __len__(self):
