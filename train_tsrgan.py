166d87b4c589e5727afe16fac4ccc6690260734c
liwanjunit <liwanjunit@163.com>
2022-04-22 23:50:45 +0800

    4.22

--- train_tsrgan.py
+++ train_tsrgan.py
@@ -18,30 +18,30 @@ from model.model_tsrgan import Generator_TSRGAN, Discriminator_TSRGAN
 if __name__ == '__main__':
 
     CROP_SIZE = 128
-    UPSCALE_FACTOR = 8
-    NUM_EPOCHS = 10
-    EPOCH_SUM = 190
+    UPSCALE_FACTOR = 2
+    NUM_EPOCHS = 5
+    EPOCH_SUM = 0
     BATCH_SIZE = 2
 
-    D_INIT_LR = 0.000001
+    D_INIT_LR = 0.0001
     G_INIT_LR = 0.0001
 
-    MODEL_NAME_G = f'tsrgan_netG_epoch_{UPSCALE_FACTOR}_190.pth'
-    MODEL_NAME_D = f'tsrgan_netD_epoch_{UPSCALE_FACTOR}_190.pth'
+    # MODEL_NAME_G = f'tsrgan_netG_epoch_{UPSCALE_FACTOR}_190.pth'
+    # MODEL_NAME_D = f'tsrgan_netD_epoch_{UPSCALE_FACTOR}_190.pth'
 
     print(f'crop_size:{CROP_SIZE}')
     print(f'epoch_sum:{EPOCH_SUM}')
     print(f'batch_size:{BATCH_SIZE}')
     print(f'upscale_factor:{UPSCALE_FACTOR}')
 
-    train_set = TrainDatasetFromFolder('/kaggle/input/data-17500/train', crop_size=CROP_SIZE, upscale_factor=UPSCALE_FACTOR)
-    val_set = ValDatasetFromFolder('/kaggle/input/data-17500/val', upscale_factor=UPSCALE_FACTOR)
+    # train_set = TrainDatasetFromFolder('/kaggle/input/data-17500/train', crop_size=CROP_SIZE, upscale_factor=UPSCALE_FACTOR)
+    # val_set = ValDatasetFromFolder('/kaggle/input/data-17500/val', upscale_factor=UPSCALE_FACTOR)
 
     # train_set = TrainDatasetFromFolder('../data_17500/train', crop_size=CROP_SIZE, upscale_factor=UPSCALE_FACTOR)
     # val_set = ValDatasetFromFolder('../data_17500/val', upscale_factor=UPSCALE_FACTOR)
 
-    # train_set = TrainDatasetFromFolder('C:/code/SRGAN-master/VOC2012/VOC2012/train', crop_size=CROP_SIZE, upscale_factor=UPSCALE_FACTOR)
-    # val_set = ValDatasetFromFolder('C:/code/SRGAN-master/VOC2012/VOC2012/val', crop_size=CROP_SIZE, upscale_factor=UPSCALE_FACTOR)
+    train_set = TrainDatasetFromFolder('/input0/data_17500/train', crop_size=CROP_SIZE, upscale_factor=UPSCALE_FACTOR)
+    val_set = ValDatasetFromFolder('/input0/data_17500/val', upscale_factor=UPSCALE_FACTOR)
 
     train_loader = DataLoader(dataset=train_set, num_workers=4, batch_size=BATCH_SIZE, shuffle=True)
     val_loader = DataLoader(dataset=val_set, num_workers=4, batch_size=1, shuffle=False)
@@ -57,11 +57,11 @@ if __name__ == '__main__':
         netG.cuda()
         netD.cuda()
         generator_criterion.cuda()
-        netG.load_state_dict(torch.load('epochs/' + MODEL_NAME_G), False)
-        netD.load_state_dict(torch.load('epochs/' + MODEL_NAME_D), False)
-    else:
-        netG.load_state_dict(torch.load('epochs/' + MODEL_NAME_G, map_location=lambda storage, loc: storage))
-        netD.load_state_dict(torch.load('epochs/' + MODEL_NAME_D, map_location=lambda storage, loc: storage))
+    #     netG.load_state_dict(torch.load('epochs/' + MODEL_NAME_G), False)
+    #     netD.load_state_dict(torch.load('epochs/' + MODEL_NAME_D), False)
+    # else:
+    #     netG.load_state_dict(torch.load('epochs/' + MODEL_NAME_G, map_location=lambda storage, loc: storage))
+    #     netD.load_state_dict(torch.load('epochs/' + MODEL_NAME_D, map_location=lambda storage, loc: storage))
 
     optimizerG = optim.Adam(netG.parameters(), lr=G_INIT_LR)
     optimizerD = optim.Adam(netD.parameters(), lr=D_INIT_LR)
