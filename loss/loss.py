26b357f14a2b873227ccc799b1eeb8f36a9d9916
liwanjunit <liwanjunit@163.com>
2022-04-24 20:06:34 +0800

    4.22

--- loss/loss.py
+++ loss/loss.py
@@ -7,8 +7,8 @@ class GeneratorLoss(nn.Module):
     def __init__(self):
         super(GeneratorLoss, self).__init__()
         vgg = vgg16(pretrained=True)
-        loss_network = nn.Sequential(*list(vgg.features)[:31]).eval()
-        # loss_network = nn.Sequential(*list(vgg.features)[:35]).eval()
+        # loss_network = nn.Sequential(*list(vgg.features)[:31]).eval()
+        loss_network = nn.Sequential(*list(vgg.features)[:35]).eval()
         for param in loss_network.parameters():
             param.requires_grad = False
         self.loss_network = loss_network
