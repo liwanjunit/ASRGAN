f77cb38bc09f354d2590ace1345f53a911bb3371
liwanjunit <liwanjunit@163.com>
2022-04-29 09:41:00 +0800

    4.29

--- train_srcnn.py
+++ train_srcnn.py
@@ -22,12 +22,13 @@ if __name__ == '__main__':
     CROP_SIZE = 128
     UPSCALE_FACTOR = 2
     NUM_EPOCHS = 100
-    EPOCH_SUM = 0
+    EPOCH_SUM = 100
 
     INIT_LR = 0.0001
     BATCH_SIZE = 2
 
     # MODEL_NAME = f'/kaggle/input/sr-model/srcnn_epoch_{UPSCALE_FACTOR}_100.pth'
+    MODEL_NAME = f'srcnn_epoch_{UPSCALE_FACTOR}_100.pth'
 
     print(f'crop_size:{CROP_SIZE}')
     print(f'epoch_sum:{EPOCH_SUM}')
@@ -54,10 +55,10 @@ if __name__ == '__main__':
     if torch.cuda.is_available():
         model.cuda()
         loss_function.cuda()
-        # model.load_state_dict(torch.load('epochs/' + MODEL_NAME), False)
+        model.load_state_dict(torch.load('epochs/' + MODEL_NAME), False)
         # model.load_state_dict(torch.load(MODEL_NAME), False)
-    # else:
-        # model.load_state_dict(torch.load('epochs/' + MODEL_NAME, map_location=lambda storage, loc: storage))
+    else:
+        model.load_state_dict(torch.load('epochs/' + MODEL_NAME, map_location=lambda storage, loc: storage))
         # model.load_state_dict(torch.load(MODEL_NAME, map_location=lambda storage, loc: storage))
 
     optimizerG = optim.Adam(model.parameters(), lr=INIT_LR)
