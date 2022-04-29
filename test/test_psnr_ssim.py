f7dbbc51850c10ad6f05b68b07f9234861b0c5c9
liwanjunit <liwanjunit@163.com>
2022-04-29 22:35:04 +0800

    4.29

--- test/test_psnr_ssim.py
+++ test/test_psnr_ssim.py
@@ -18,15 +18,15 @@ from model.model_srcnn import SRCNN
 
 if __name__ == '__main__':
 
-    UPSCALE_FACTOR = 4
+    UPSCALE_FACTOR = 2
 
     TEST_DIR = f'../data/test_x{UPSCALE_FACTOR}'
 
-    # MODEL = 'srcnn'
+    MODEL = 'srcnn'
     # MODEL = 'srresnet'
     # MODEL = 'srgan'
     # MODEL = 'tsrgan'
-    MODEL = 'tsrgan_mse'
+    # MODEL = 'tsrgan_mse'
     # MODEL = 'tsrgan_v2'
 
     epoch_sum = 1
@@ -39,17 +39,18 @@ if __name__ == '__main__':
     test_loader = DataLoader(dataset=test_set, num_workers=4, batch_size=1, shuffle=False)
     test_bar = tqdm(test_loader, desc='[testing benchmark datasets]')
 
-    for i in range(50):
+    for i in range(200):
 
         index = 1
         psnr_sum = 0
         ssim_sum = 0
 
-        MODEL_NAME = f'C:/code/train_results/new_model/x{UPSCALE_FACTOR}/{MODEL}_x{UPSCALE_FACTOR}/G/{MODEL}_netG_epoch_{UPSCALE_FACTOR}_{i+150}.pth'
+        # MODEL_NAME = f'C:/code/train_results/new_model/x{UPSCALE_FACTOR}/{MODEL}_x{UPSCALE_FACTOR}/G/{MODEL}_netG_epoch_{UPSCALE_FACTOR}_{i+1}.pth'
+        MODEL_NAME = f'C:/code/train_results/new_model/x{UPSCALE_FACTOR}/{MODEL}_x{UPSCALE_FACTOR}/model/{MODEL}_epoch_{UPSCALE_FACTOR}_{i+1}.pth'
 
-        model = Generator_TSRGAN(UPSCALE_FACTOR).eval()
+        # model = Generator_TSRGAN(UPSCALE_FACTOR).eval()
         # model = Generator(UPSCALE_FACTOR).eval()
-        # model = SRCNN().eval()
+        model = SRCNN().eval()
         if torch.cuda.is_available():
             model = model.cuda()
         model.load_state_dict(torch.load(MODEL_NAME), False)
