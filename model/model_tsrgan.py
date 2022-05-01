40d6eba2f432c378e0e55f06c257679384b3b1cf
liwanjunit <liwanjunit@163.com>
2022-05-02 00:33:22 +0800

    4.29

--- model/model_tsrgan.py
+++ model/model_tsrgan.py
@@ -18,12 +18,11 @@ class Generator_TSRGAN(nn.Module):
         self.block4 = ResidualBlock(64)
         self.block5 = ResidualBlock(64)
         self.block6 = ResidualBlock(64)
-        self.block7 = ResidualBlock(64)
-        self.block8 = ResidualBlock(64)
-        self.block9 = ResidualBlock(64)
+        # self.block7 = ResidualBlock(64)
+        # self.block8 = ResidualBlock(64)
+        # self.block9 = ResidualBlock(64)
         self.block10 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64)
+            nn.Conv2d(64, 64, kernel_size=3, padding=1)
         )
         block11 = [UpsampleBLock(64, 2) for _ in range(upsample_block_num)]
         block11.append(nn.Conv2d(64, 3, kernel_size=9, padding=4))
@@ -37,9 +36,9 @@ class Generator_TSRGAN(nn.Module):
         block4 = self.block4(block3)
         block5 = self.block5(block4)
         block6 = self.block6(block5)
-        block7 = self.block7(block6)
-        block8 = self.block8(block7)
-        block9 = self.block9(block8)
+        # block7 = self.block7(block6)
+        # block8 = self.block8(block7)
+        # block9 = self.block9(block8)
         block10 = self.block10(block6)
         block11 = self.block11(block1 + block10)
         return (torch.tanh(block11) + 1) / 2
