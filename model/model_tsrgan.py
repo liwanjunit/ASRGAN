60aa7eb87447b9d607c00a6b17888d0ddd6a9e3e
liwanjunit <liwanjunit@163.com>
2022-05-02 09:45:58 +0800

    4.29

--- model/model_tsrgan.py
+++ model/model_tsrgan.py
@@ -21,12 +21,12 @@ class Generator_TSRGAN(nn.Module):
         # self.block7 = ResidualBlock(64)
         # self.block8 = ResidualBlock(64)
         # self.block9 = ResidualBlock(64)
-        self.block10 = nn.Sequential(
+        self.block7 = nn.Sequential(
             nn.Conv2d(64, 64, kernel_size=3, padding=1)
         )
-        block11 = [UpsampleBLock(64, 2) for _ in range(upsample_block_num)]
-        block11.append(nn.Conv2d(64, 3, kernel_size=9, padding=4))
-        self.block11 = nn.Sequential(*block11)
+        block8 = [UpsampleBLock(64, 2) for _ in range(upsample_block_num)]
+        block8.append(nn.Conv2d(64, 3, kernel_size=9, padding=4))
+        self.block8 = nn.Sequential(*block8)
 
     def forward(self, x):
 
@@ -39,9 +39,9 @@ class Generator_TSRGAN(nn.Module):
         # block7 = self.block7(block6)
         # block8 = self.block8(block7)
         # block9 = self.block9(block8)
-        block10 = self.block10(block6)
-        block11 = self.block11(block1 + block10)
-        return (torch.tanh(block11) + 1) / 2
+        block7 = self.block7(block6)
+        block8 = self.block8(block1 + block7)
+        return (torch.tanh(block8) + 1) / 2
 
 
 class Discriminator_TSRGAN(nn.Module):
