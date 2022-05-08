a3dbd28226c204ea110202fdd7f480b3c3c49d81
liwanjunit <liwanjunit@163.com>
2022-05-09 00:09:03 +0800

    5.8

---
+++ model/model_asrgan.py
@@ -0,0 +1,125 @@
+import math
+import torch
+from torch import nn
+from attention import Attention, InterlacedSparseSelfAttention
+
+
+class Generator_TSRGAN(nn.Module):
+    def __init__(self, scale_factor):
+        upsample_block_num = int(math.log(scale_factor, 2))
+
+        super(Generator_TSRGAN, self).__init__()
+        self.block1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=9, padding=4),
+            nn.PReLU()
+        )
+        self.block2 = ResidualBlock(64)
+        self.block3 = ResidualBlock(64)
+        self.block4 = ResidualBlock(64)
+        self.block5 = ResidualBlock(64)
+        self.block6 = ResidualBlock(64)
+        self.block7 = ResidualBlock(64)
+        self.block8 = ResidualBlock(64)
+        self.block9 = ResidualBlock(64)
+        self.block10 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        )
+        block8 = [UpsampleBLock(64, 2) for _ in range(upsample_block_num)]
+        block8.append(nn.Conv2d(64, 3, kernel_size=9, padding=4))
+        self.block8 = nn.Sequential(*block8)
+
+    def forward(self, x):
+
+        block1 = self.block1(x)
+        block2 = self.block2(block1)
+        block3 = self.block3(block2)
+        block4 = self.block4(block3)
+        block5 = self.block5(block4)
+        block6 = self.block6(block5)
+        block7 = self.block4(block6)
+        block8 = self.block5(block7)
+        block9 = self.block6(block8)
+        block10 = self.block7(block9)
+        block11 = self.block8(block1 + block10)
+        return (torch.tanh(block11) + 1) / 2
+
+
+class Discriminator_TSRGAN(nn.Module):
+    def __init__(self):
+        super(Discriminator_TSRGAN, self).__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2),
+
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(512, 1024, kernel_size=1),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(1024, 1, kernel_size=1)
+        )
+
+    def forward(self, x):
+        batch_size = x.size(0)
+        return torch.sigmoid(self.net(x).view(batch_size))
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, channels):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(channels, channels, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(channels, channels, kernel_size=3, padding=1)
+        self.prelu = nn.PReLU()
+        # self.attention = Attention(dim=64)
+        self.attention = InterlacedSparseSelfAttention(dim=64, P_h=8, P_w=8)
+
+    def forward(self, x):
+
+        x1 = self.prelu(self.conv1(x))
+        # print(f'x.shape: {x.shape}')
+        # print(f'x1.shape: {x1.shape}')
+        x2 = self.prelu(self.conv1(x + x1))
+        x3 = self.attention(x + x1 + x2)
+        # residual = self.conv2(residual)
+
+        return x + x3 * 0.5
+
+
+class UpsampleBLock(nn.Module):
+    def __init__(self, in_channels, up_scale):
+        super(UpsampleBLock, self).__init__()
+        self.conv = nn.Conv2d(in_channels, in_channels * up_scale ** 2, kernel_size=3, padding=1)
+        self.pixel_shuffle = nn.PixelShuffle(up_scale)
+        self.prelu = nn.PReLU()
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.pixel_shuffle(x)
+        x = self.prelu(x)
+        return x
---
+++ train_asrgan.py
@@ -0,0 +1,200 @@
+
+import os
+from math import log10
+
+import pandas as pd
+import torch.optim as optim
+import torch.utils.data
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision.transforms import ToPILImage
+from tqdm import tqdm
+
+import pytorch_ssim
+from data_utils import TrainDatasetFromFolder, ValDatasetFromFolder
+from loss.loss_new import GeneratorLoss_NEW
+from model.model_asrgan import Generator_TSRGAN, Discriminator_TSRGAN
+
+if __name__ == '__main__':
+
+    CROP_SIZE = 128
+    UPSCALE_FACTOR = 4
+    NUM_EPOCHS = 30
+    EPOCH_SUM = 10
+    BATCH_SIZE = 2
+
+    D_INIT_LR = 0.0001
+    G_INIT_LR = 0.0001
+
+    MODEL_NAME_G = f'asrgan_netG_epoch_{UPSCALE_FACTOR}_10.pth'
+    MODEL_NAME_D = f'asrgan_netD_epoch_{UPSCALE_FACTOR}_10.pth'
+
+    print(f'crop_size:{CROP_SIZE}')
+    print(f'epoch_sum:{EPOCH_SUM}')
+    print(f'batch_size:{BATCH_SIZE}')
+    print(f'upscale_factor:{UPSCALE_FACTOR}')
+
+    # train_set = TrainDatasetFromFolder('/kaggle/input/data-17500/train', crop_size=CROP_SIZE, upscale_factor=UPSCALE_FACTOR)
+    # val_set = ValDatasetFromFolder('/kaggle/input/data-17500/val', upscale_factor=UPSCALE_FACTOR)
+
+    train_set = TrainDatasetFromFolder('../data_17500/train', crop_size=CROP_SIZE, upscale_factor=UPSCALE_FACTOR)
+    val_set = ValDatasetFromFolder('../data_17500/val', upscale_factor=UPSCALE_FACTOR)
+
+    # train_set = TrainDatasetFromFolder('../input/input0/data_17500/train', crop_size=CROP_SIZE, upscale_factor=UPSCALE_FACTOR)
+    # val_set = ValDatasetFromFolder('../input/input0/data_17500/val', upscale_factor=UPSCALE_FACTOR)
+
+    train_loader = DataLoader(dataset=train_set, num_workers=4, batch_size=BATCH_SIZE, shuffle=True)
+    val_loader = DataLoader(dataset=val_set, num_workers=4, batch_size=1, shuffle=False)
+
+    netG = Generator_TSRGAN(UPSCALE_FACTOR)
+    print('# generator parameters:', sum(param.numel() for param in netG.parameters()))
+    netD = Discriminator_TSRGAN()
+    print('# discriminator parameters:', sum(param.numel() for param in netD.parameters()))
+
+    generator_criterion = GeneratorLoss_NEW()
+
+    if torch.cuda.is_available():
+        netG.cuda()
+        netD.cuda()
+        generator_criterion.cuda()
+        netG.load_state_dict(torch.load('epochs/' + MODEL_NAME_G), False)
+        netD.load_state_dict(torch.load('epochs/' + MODEL_NAME_D), False)
+    else:
+        netG.load_state_dict(torch.load('epochs/' + MODEL_NAME_G, map_location=lambda storage, loc: storage))
+        netD.load_state_dict(torch.load('epochs/' + MODEL_NAME_D, map_location=lambda storage, loc: storage))
+
+    optimizerG = optim.Adam(netG.parameters(), lr=G_INIT_LR)
+    optimizerD = optim.Adam(netD.parameters(), lr=D_INIT_LR)
+
+    results = {'d_loss': [], 'g_loss': [], 'd_score': [], 'g_score': [], 'psnr': [], 'ssim': []}
+
+    for epoch in range(1, NUM_EPOCHS + 1):
+        train_bar = tqdm(train_loader)
+        running_results = {'batch_sizes': 0, 'd_loss': 0, 'g_loss': 0, 'd_score': 0, 'g_score': 0}
+
+        netG.train()
+        netD.train()
+        for data, _, target in train_bar:
+            g_update_first = True
+            batch_size = data.size(0)
+            running_results['batch_sizes'] += batch_size
+
+            ############################
+            # (1) Update D network: maximize D(x)-1-D(G(z))
+            ###########################
+            real_img = Variable(target)
+            if torch.cuda.is_available():
+                real_img = real_img.cuda()
+            z = Variable(data)
+            if torch.cuda.is_available():
+                z = z.cuda()
+            fake_img = netG(z)
+
+            netD.zero_grad()
+            real_out = netD(real_img).mean()
+            fake_out = netD(fake_img).mean()
+            d_loss = 1 - real_out + fake_out
+            d_loss.backward(retain_graph=True)
+            # d_loss.backward()
+            optimizerD.step()
+
+            ############################
+            # (2) Update G network: minimize 1-D(G(z)) + Perception Loss + Image Loss + TV Loss
+            ###########################
+            netG.zero_grad()
+            ## The two lines below are added to prevent runetime error in Google Colab ##
+            fake_img = netG(z)
+            fake_out = netD(fake_img).mean()
+            ##
+            g_loss = generator_criterion(fake_out, fake_img, real_img)
+            g_loss.backward()
+
+            fake_img = netG(z)
+            fake_out = netD(fake_img).mean()
+
+            optimizerG.step()
+
+            # loss for current batch before optimization
+            running_results['g_loss'] += g_loss.item() * batch_size
+            running_results['d_loss'] += d_loss.item() * batch_size
+            running_results['d_score'] += real_out.item() * batch_size
+            running_results['g_score'] += fake_out.item() * batch_size
+
+            train_bar.set_description(desc='[%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f' % (
+                epoch, NUM_EPOCHS, running_results['d_loss'] / running_results['batch_sizes'],
+                running_results['g_loss'] / running_results['batch_sizes'],
+                running_results['d_score'] / running_results['batch_sizes'],
+                running_results['g_score'] / running_results['batch_sizes']))
+
+        netG.eval()
+        out_path = 'training_results/asrgan_SRF_' + str(UPSCALE_FACTOR) + '/'
+        if not os.path.exists(out_path):
+            os.makedirs(out_path)
+
+        with torch.no_grad():
+            val_bar = tqdm(val_loader)
+            valing_results = {'mse': 0, 'ssims': 0, 'psnr': 0, 'ssim': 0, 'batch_sizes': 0}
+            val_images = []
+            image_index = 1
+            for val_lr, val_hr_restore, val_hr in val_bar:
+                batch_size = val_lr.size(0)
+                valing_results['batch_sizes'] += batch_size
+                lr = Variable(val_lr)
+                hr = Variable(val_hr)
+                if torch.cuda.is_available():
+                    lr = lr.cuda()
+                    hr = hr.cuda()
+                sr = netG(lr)
+
+                if image_index == 343:
+                    sr_image = ToPILImage()(sr[0].data.cpu())
+                    sr_image.save('test_image/results/' + 'asrgan_%d_%d.png' % (UPSCALE_FACTOR, epoch + EPOCH_SUM))
+
+                image_index += 1
+
+                batch_mse = ((sr - hr) ** 2).data.mean()
+                valing_results['mse'] += batch_mse * batch_size
+                batch_ssim = pytorch_ssim.ssim(sr, hr).item()
+                valing_results['ssims'] += batch_ssim * batch_size
+                valing_results['psnr'] = 10 * log10(
+                    (hr.max() ** 2) / (valing_results['mse'] / valing_results['batch_sizes']))
+                valing_results['ssim'] = valing_results['ssims'] / valing_results['batch_sizes']
+                val_bar.set_description(
+                    desc='[converting LR images to SR images] PSNR: %.4f dB SSIM: %.4f' % (
+                        valing_results['psnr'], valing_results['ssim']))
+
+            #     val_images.extend(
+            #         [display_transform()(val_hr_restore.squeeze(0)), display_transform()(hr.data.cpu().squeeze(0)),
+            #          display_transform()(sr.data.cpu().squeeze(0))])
+            #
+            # val_images = torch.stack(val_images)
+            # val_images = torch.chunk(val_images, val_images.size(0) // 15)
+            # val_save_bar = tqdm(val_images, desc='[saving training results]')
+            #
+            # index = 1
+            # for image in val_save_bar:
+            #     image = utils.make_grid(image, nrow=3, padding=5)
+            #     utils.save_image(image, out_path + 'tsrgan_epoch_%d_index_%d.png' % (epoch + EPOCH_SUM, index), padding=5)
+            #     index += 1
+            #     break
+
+        # save model parameters
+        if epoch % 1 == 0 and epoch != 0:
+            torch.save(netG.state_dict(), 'epochs/asrgan_netG_epoch_%d_%d.pth' % (UPSCALE_FACTOR, epoch + EPOCH_SUM))
+            torch.save(netD.state_dict(), 'epochs/asrgan_netD_epoch_%d_%d.pth' % (UPSCALE_FACTOR, epoch + EPOCH_SUM))
+
+        # save loss\scores\psnr\ssim
+        results['d_loss'].append(running_results['d_loss'] / running_results['batch_sizes'])
+        results['g_loss'].append(running_results['g_loss'] / running_results['batch_sizes'])
+        results['d_score'].append(running_results['d_score'] / running_results['batch_sizes'])
+        results['g_score'].append(running_results['g_score'] / running_results['batch_sizes'])
+        results['psnr'].append(valing_results['psnr'])
+        results['ssim'].append(valing_results['ssim'])
+
+        if epoch % 1 == 0 and epoch != 0:
+            out_path = 'statistics/'
+            data_frame = pd.DataFrame(
+                data={'Loss_D': results['d_loss'], 'Loss_G': results['g_loss'], 'Score_D': results['d_score'],
+                      'Score_G': results['g_score'], 'PSNR': results['psnr'], 'SSIM': results['ssim']},
+                index=range(1, epoch + 1))
+            data_frame.to_csv(out_path + 'asrgan_train_' + str(UPSCALE_FACTOR) + f'_{epoch + EPOCH_SUM}.csv', index_label='Epoch')
