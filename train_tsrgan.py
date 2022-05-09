f33c645bac47014cc37b8c24f8a3dc54a0122a4d
liwanjunit <liwanjunit@163.com>
2022-05-09 10:54:35 +0800

    5.9

--- train_tsrgan.py
+++ train_tsrgan.py
@@ -20,14 +20,14 @@ if __name__ == '__main__':
     CROP_SIZE = 128
     UPSCALE_FACTOR = 4
     NUM_EPOCHS = 20
-    EPOCH_SUM = 100
+    EPOCH_SUM = 90
     BATCH_SIZE = 2
 
     D_INIT_LR = 0.0001
     G_INIT_LR = 0.0001
 
-    MODEL_NAME_G = f'tsrgan_netG_epoch_{UPSCALE_FACTOR}_100.pth'
-    MODEL_NAME_D = f'tsrgan_netD_epoch_{UPSCALE_FACTOR}_100.pth'
+    MODEL_NAME_G = f'tsrgan_netG_epoch_{UPSCALE_FACTOR}_90.pth'
+    MODEL_NAME_D = f'tsrgan_netD_epoch_{UPSCALE_FACTOR}_90.pth'
 
     print(f'crop_size:{CROP_SIZE}')
     print(f'epoch_sum:{EPOCH_SUM}')
