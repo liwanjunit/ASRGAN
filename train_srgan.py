298673a2b2c7ecddc2e0b8882f4bd25d601b5fa0
liwanjunit <liwanjunit@163.com>
2022-05-20 10:52:26 +0800

    5.20

--- train_srgan.py
+++ train_srgan.py
@@ -20,14 +20,14 @@ if __name__ == '__main__':
     CROP_SIZE = 128
     UPSCALE_FACTOR = 2
     NUM_EPOCHS = 5
-    EPOCH_SUM = 90
+    EPOCH_SUM = 95
     BATCH_SIZE = 2
 
     D_INIT_LR = 0.0001
     G_INIT_LR = 0.0001
 
-    MODEL_NAME_G = f'srgan_netG_epoch_{UPSCALE_FACTOR}_90.pth'
-    MODEL_NAME_D = f'srgan_netD_epoch_{UPSCALE_FACTOR}_90.pth'
+    MODEL_NAME_G = f'srgan_netG_epoch_{UPSCALE_FACTOR}_95.pth'
+    MODEL_NAME_D = f'srgan_netD_epoch_{UPSCALE_FACTOR}_95.pth'
 
     print(f'crop_size:{CROP_SIZE}')
     print(f'epoch_sum:{EPOCH_SUM}')
