87923cc27445c2569d352e401f1d3457ab69d94a
liwanjunit <liwanjunit@163.com>
2022-05-24 00:38:49 +0800

    5.24

--- train_asrgan.py
+++ train_asrgan.py
@@ -18,7 +18,7 @@ from model.model_asrgan import Generator_ASRGAN, Discriminator_ASRGAN
 if __name__ == '__main__':
 
     CROP_SIZE = 128
-    UPSCALE_FACTOR = 8
+    UPSCALE_FACTOR = 4
     NUM_EPOCHS = 25
     EPOCH_SUM = 0
     BATCH_SIZE = 2
