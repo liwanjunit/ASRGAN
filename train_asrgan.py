609e714aef8c5bf71510ac2160302fb02fda1fd2
liwanjunit <liwanjunit@163.com>
2022-05-24 20:55:51 +0800

    5.24

--- train_asrgan.py
+++ train_asrgan.py
@@ -18,16 +18,16 @@ from model.model_asrgan import Generator_ASRGAN, Discriminator_ASRGAN
 if __name__ == '__main__':
 
     CROP_SIZE = 128
-    UPSCALE_FACTOR = 8
+    UPSCALE_FACTOR = 4
     NUM_EPOCHS = 25
-    EPOCH_SUM = 25
+    EPOCH_SUM = 50
     BATCH_SIZE = 2
 
     D_INIT_LR = 0.0001
     G_INIT_LR = 0.0001
 
-    MODEL_NAME_G = f'asrgan_netG_epoch_{UPSCALE_FACTOR}_25.pth'
-    MODEL_NAME_D = f'asrgan_netD_epoch_{UPSCALE_FACTOR}_25.pth'
+    MODEL_NAME_G = f'asrgan_netG_epoch_{UPSCALE_FACTOR}_50.pth'
+    MODEL_NAME_D = f'asrgan_netD_epoch_{UPSCALE_FACTOR}_50.pth'
     # MODEL_NAME_G = f'asrgan_netG_epoch_2_100.pth'
     # MODEL_NAME_D = f'asrgan_netD_epoch_2_100.pth'
 
