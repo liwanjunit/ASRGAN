5fc688c0877fbccd726f42cd51d2853ee4aec213
liwanjunit <liwanjunit@163.com>
2022-06-15 09:26:23 +0800

    6.13

--- train_sasrgan.py
+++ train_sasrgan.py
@@ -18,9 +18,9 @@ from model.model_sasrgan import Generator_SASRGAN, Discriminator_SASRGAN
 if __name__ == '__main__':
 
     CROP_SIZE = 128
-    UPSCALE_FACTOR = 4
-    NUM_EPOCHS = 15
-    EPOCH_SUM = 85
+    UPSCALE_FACTOR = 8
+    NUM_EPOCHS = 11
+    EPOCH_SUM = 89
     BATCH_SIZE = 2
 
     D_INIT_LR = 0.0001
