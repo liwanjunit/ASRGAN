c523b06f53af8067b1f402d1e2ab65a634197964
liwanjunit <liwanjunit@163.com>
2022-06-15 14:42:47 +0800

    6.13

--- train_sasrgan.py
+++ train_sasrgan.py
@@ -19,8 +19,8 @@ if __name__ == '__main__':
 
     CROP_SIZE = 128
     UPSCALE_FACTOR = 4
-    NUM_EPOCHS = 8
-    EPOCH_SUM = 92
+    NUM_EPOCHS = 10
+    EPOCH_SUM = 90
     BATCH_SIZE = 2
 
     D_INIT_LR = 0.0001
